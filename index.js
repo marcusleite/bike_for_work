b40b00223c0137c5328c60fcdfc59b6b96a89c36
Marcus Leite <leite.marcus@gmail.com>
2017-07-25 23:21:46 -0300

    Fixing the requrires; Changing the TODO list, reidentation, removing the division by two  in the final result.

--- index.js
+++ index.js
@@ -4,23 +4,17 @@
 const fs = require('fs');
 
 // App libraries
+const config = require('./config');
 const getActivities = require('./activities_get');
+const is_a_trip_to_work = require('./geo');
 const loadActivities = require('./activities_load');
-const geo = require('./geo');
-
-// Third parties
-const config = require('config.json')('./config.json');
 
 /* TODO LIST
-    * Decide what to put in the configuration file.
     * Count arrivals and departures.
-    * Oauth login
+    * Oauth login(passport-strava)
     * Pick workplace from Google Maps widget or similar
-    * async/await version
 */
 
-// Config
-//const str_activitiesFolder = config.activities_folder;
 var num_trips = 0;
 var arr_activities = [];
 
@@ -29,29 +23,29 @@ if (!fs.existsSync(config.activities_folder)) {
     fs.mkdirSync(config.activities_folder);
 }
 
-getActivities.getActivities(config.activities_folder, 1)
-    .then(        
-        () => {
-            // Runs when the downloads finishes sucessfully.            
-            arr_activities = loadActivities.loadActivities(config.activities_folder, 20);
-            arr_activities.forEach(function (JSON_activity) {                
-                if (JSON_activity.type === 'Ride') {
-                    if (geo.its_a_trip_to_work(JSON_activity)) {
-                        num_trips++;
-                    } else {
-                        console.log(
-                            "Activity that wasn't to work: id " + JSON_activity.id 
-                            //+ " Coords: " + JSON_activity.start_latlng 
-                            + " Start date Local: " + JSON_activity.start_date_local
-                            + " Activity Name: " + JSON_activity.name                            
-                        );
-                    }
+getActivities(config.activities_folder, 1)
+    .then(
+    () => {
+        // Runs when the downloads finishes sucessfully.            
+        arr_activities = loadActivities(config.activities_folder, 20);
+        arr_activities.forEach(function (JSON_activity) {
+            if (JSON_activity.type === 'Ride') {
+                if (is_a_trip_to_work(JSON_activity)) {
+                    num_trips++;
+                } else if (!JSON_activity.name.startsWith("Volta")) {                    
+                    console.log(
+                        "Activity that wasn't to work: id " + JSON_activity.id
+                        //+ " Coords: " + JSON_activity.start_latlng 
+                        + " Time:  " + JSON_activity.start_date_local
+                        + " Activity Name: " + JSON_activity.name
+                    );
                 }
-            });
-            console.log("Total trips to work: " + num_trips / 2);
-        }
-    ).catch(        
-        (err) => {
-            console.log(err);
-        }
+            }
+        });
+        console.log("Total trips from work: " + num_trips);
+    }
+    ).catch(
+    (err) => {
+        console.log(err);
+    }
     );
