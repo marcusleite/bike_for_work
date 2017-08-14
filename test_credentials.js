592d8582b0f2d5c39614fa14fd324dd951a8fe7d
Marcus Leite <leite.marcus@gmail.com>
2017-08-13 21:53:53 -0300

    Test credentials now uses env variables
    Test credentials now uses env variables

--- test_credentials.js
+++ test_credentials.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const strava = require('strava-v3');
+// Loading env folders for development
+require('dotenv').config();
 
 // Script to test Strava API credentials.
 strava.athlete.get({}, function (err, payload, limits) {
